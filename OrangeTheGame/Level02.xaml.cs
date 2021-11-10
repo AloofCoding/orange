ed6fbde0898403e91f3a2391403759fcf23af22f
Andreas Steiner <andreas.steiner@hak-kitz.ac.at>
2021-11-10 11:05:29 +0100

    fixing integerupdown controls, still wrong event used

--- OrangeTheGame/Level02.xaml.cs
+++ OrangeTheGame/Level02.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Threading;
+using Xceed.Wpf.Toolkit;
 
 namespace OrangeTheGame
 {
@@ -25,7 +26,16 @@ namespace OrangeTheGame
             InitializeComponent();
         }
 
-        public bool hexcode = false;
         public string hexstring;
+
+        private void iup_hex1_TouchUp(object sender, KeyEventArgs e)
+        {
+            hexstring = "#FF" + iup_hex1.Value.ToString() + "F" + iup_hex2.Value.ToString() + "2";
+            Color color = (Color)ColorConverter.ConvertFromString(hexstring);
+            SolidColorBrush myBrush = new SolidColorBrush(color);
+            this.Background = myBrush;
+            iup_hex1.Background = myBrush;
+            iup_hex2.Background = myBrush;
+        }
     }
 }
