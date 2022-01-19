1bb1af5d40fae8f116e474ef164e8f5be84be972
Andreas Steiner <andreas.steiner@hak-kitz.ac.at>
2022-01-19 11:05:55 +0100

    new approach for level 8 with 2d array and thread sleep

--- OrangeTheGame/Level08.xaml.cs
+++ OrangeTheGame/Level08.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace OrangeTheGame
 {
@@ -28,57 +29,45 @@ namespace OrangeTheGame
 
         private void create_button()
         {
-            //int i = 1;
-            //string btn_name = "btn_" + i;
-
             Color color = (Color)ColorConverter.ConvertFromString("#CE7300");
             SolidColorBrush brush = new SolidColorBrush(color);
 
-            //Random rnd_x = new Random();
-            //int x_coord = rnd_x.Next(0, 714);
-
-            //Random rnd_y = new Random();
-            //int y_coord = rnd_y.Next(0, 314);
-
             Button btn = new Button();
-            //btn.Name = btn_name;
+
             btn.Width = 50;
             btn.Height = 50;
             btn.Background = brush;
-            //btn.BorderThickness = new Thickness(0, 0, 0, 0);
-            //btn.Margin = new Thickness(x_coord, y_coord, 714 - x_coord, 314 - y_coord);
+
             btn.Click += btn_Click;
+
             btn.SetValue(Grid.RowProperty, new Random().Next(0, grid_l8.RowDefinitions.Count()));
+            Thread.Sleep(new Random().Next(0,1000));
             btn.SetValue(Grid.ColumnProperty, new Random().Next(0, grid_l8.ColumnDefinitions.Count()));
+
+            int[,] arr_grid = new int[10, 10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int i2 = 0; i2 < 10; i2++)
+                {
+                    bool temp = false;
+                    arr_grid.SetValue(temp, i, i2);
+                }
+            }
+
+            //MessageBox.Show(btn.GetValue(Grid.RowProperty).ToString());
+            //MessageBox.Show(btn.GetValue(Grid.ColumnProperty).ToString());
+
             grid_l8.Children.Add(btn);
-            //MessageBox.Show(x_coord + " | " + y_coord + " || " + (714 - x_coord) + " | " + (314 - y_coord));
-            //i++;
+            object table_x = btn.GetValue(Grid.ColumnProperty);
+            object table_y = btn.GetValue(Grid.RowProperty);
+
+            arr_grid.SetValue(true, Int32.Parse(table_x.ToString()), Int32.Parse(table_y.ToString()));
         }
 
         private void btn_Click(object sender, RoutedEventArgs e)
         {
             create_button();
-
-            /*Color color = (Color)ColorConverter.ConvertFromString("#CE7300");
-            SolidColorBrush brush = new SolidColorBrush(color);
-
-            Random rnd_x = new Random();
-            int x_coord = rnd_x.Next(0, 714);
-
-            Random rnd_y = new Random();
-            int y_coord = rnd_y.Next(0, 314);
-
-            Button btn_2 = new Button();
-            btn_2.HorizontalAlignment = HorizontalAlignment.Left;
-            btn_2.VerticalAlignment = VerticalAlignment.Top;
-            btn_2.Width = 70;
-            btn_2.Height = 70;
-            btn_2.BorderThickness = new Thickness(0, 0, 0, 0);
-            btn_2.Background = brush;
-            btn_2.Margin = new Thickness(x_coord, y_coord,0,0, 714 - x_coord, 314 - y_coord);
-            btn_2.Click += btn_Click;
-            grid_l8.Children.Add(btn_2);
-            */
         }
     }
 }
