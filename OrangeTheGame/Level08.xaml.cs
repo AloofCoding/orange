0d7c467b444605792427882b25332dc005ceba94
Andreas Steiner <andreas.steiner@hak-kitz.ac.at>
2022-03-09 11:05:10 +0100

    improving performence of level 8

--- OrangeTheGame/Level08.xaml.cs
+++ OrangeTheGame/Level08.xaml.cs
@@ -24,29 +24,42 @@ namespace OrangeTheGame
         {
             InitializeComponent();
 
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < 25; i++)
             {
-                create_button();
-                counter++;
+                //create_button();
+                counter++;               
             }
+
+            //if (btn_counter == 90)
+            //{
+            //    for (int i = 0; i < 20; i++)
+            //    {
+            //        create_button();
+            //        counter++;
+            //    }
+            //}
+
+            Random temp_rnd = new Random();
+
+            order_rnd = btn_list.OrderBy(x => temp_rnd.Next()).ToList();
             
-            if (btn_counter == 90)
-            {
-                for (int i = 0; i < 20; i++)
-                {
-                    create_button();
-                    counter++;
-                }
-            }
+            MessageBox.Show(string.Join(Environment.NewLine, order_rnd));
         }
 
-        bool[,] arr_grid = new bool[10, 10];
-        int rnd_rows = 0;
-        int rnd_cols = 0;
+        bool[,] arr_grid = new bool[7, 7];
+        //List<Button> clicked = new List<Button>();
+        //List<Button> unclicked = new List<Button>();
+        List<int> btn_list = Enumerable.Range(1,49).ToList();
+        List<int> order_rnd = new List<int>();
+        //int rnd_rows = 0;
+        //int rnd_cols = 0;
+        int btn_x;
+        int btn_y;
         int counter = 0;
         int colorcounter = 0;
         int btn_counter = 0;
         int btn_delayed = -1;
+        int temp_i = 1;
 
         Color color = (Color)ColorConverter.ConvertFromString("#FF8F02");
 
@@ -63,14 +76,14 @@ namespace OrangeTheGame
             //style = null
             btn.Style = (Style)Resources["ButtonStyle"];
 
-            btn.Width = grid_l8.Width/10;
-            btn.Height = grid_l8.Height/10;
+            btn.Width = grid_l8.Width/grid_l8.ColumnDefinitions.Count;
+            btn.Height = grid_l8.Height/grid_l8.RowDefinitions.Count;
             btn.Background = brush;
             btn.BorderBrush = brush;
             btn.Tag = btn_counter;
             btn.BorderThickness = new Thickness(0, 0, 0, 0);
 
-            if (counter == 49)
+            if (counter == 24)
             {
                 btn.Click += btn_Click;
 
@@ -82,12 +95,13 @@ namespace OrangeTheGame
 
                 //MessageBox.Show("Click-Event included.");
             }
-            else if (counter > 49)
+            else if (counter > 24)
             {
                 btn.Click += btn_Click;
                 colorcounter++;
             }
 
+            /*
             rnd_rows = new Random().Next(0, grid_l8.RowDefinitions.Count());
             btn.SetValue(Grid.RowProperty, rnd_rows);
 
@@ -95,6 +109,30 @@ namespace OrangeTheGame
 
             rnd_cols = new Random().Next(0, grid_l8.ColumnDefinitions.Count());
             btn.SetValue(Grid.ColumnProperty, rnd_cols);
+
+            btn.SetValue(Grid.RowProperty, temp_i);
+            btn.SetValue(Grid.ColumnProperty, temp_i);
+            */
+
+            switch (order_rnd[temp_i])
+            {
+                case 1:
+                    btn_x = 0;
+                    btn_y = 0;
+                    break;
+                case 2:
+                    btn_x = 1;
+                    btn_y = 0;
+                    break;
+                case 3:
+                    btn_x = 2;
+                    btn_y = 0;
+                    break;
+                case 4:
+                    btn_x = 3;
+                    btn_y = 0;
+                    break;
+            }
 
             do
             {
@@ -104,13 +142,14 @@ namespace OrangeTheGame
                     //MessageBox.Show(btn_counter.ToString());
                     btn_counter++;
                     btn_delayed++;
-                    arr_grid[rnd_cols, rnd_rows] = true;
+                    arr_grid[/*rnd_cols*/, /*rnd_rows*/] = true;
                     return;
                 }
                 else
                 {
                     //MessageBox.Show("Button couldn't be placed.");
 
+                    /*
                     rnd_rows = new Random().Next(0, grid_l8.RowDefinitions.Count());
                     btn.SetValue(Grid.RowProperty, rnd_rows);
 
@@ -118,6 +157,7 @@ namespace OrangeTheGame
 
                     rnd_cols = new Random().Next(0, grid_l8.ColumnDefinitions.Count());
                     btn.SetValue(Grid.ColumnProperty, rnd_cols);
+                    */
 
                     //MessageBox.Show(rnd_cols.ToString() + ";" + rnd_rows.ToString());
                 } 
